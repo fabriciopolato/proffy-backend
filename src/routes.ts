515d6893400c1a9c80c25c5147cbc40c65723953
Fabricio Polato <fabriciopolato16@yahoo.com.br>
2020-08-06 16:19:13 -0300

    refactor: add class to classes db

--- src/routes.ts
+++ src/routes.ts
@@ -6,11 +6,19 @@ const routes = express.Router();
 routes.post("/classes", async (request, response) => {
   const { name, avatar, whatsapp, bio, subject, cost, schedule } = request.body;
 
-  await db("users").insert({
+  const insertedUsersIds = await db("users").insert({
     name,
     avatar,
     whatsapp,
     bio,
+  });
+
+  const user_id = insertedUsersIds[0];
+
+  await db("classes").insert({
+    subject,
+    cost,
+    user_id,
   });
 
   return response.send();
